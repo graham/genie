6ae5ee7c7ecd0960c8471531a8bd5fed2447e8a1
graham <graham.abbott@gmail.com>
2010-09-15 01:15:56 -0700

    major updates, including the ability to draw in data from remote urls, as well as the ability to do proper for loops.

--- jsninja.js
+++ jsninja.js
@@ -1,5 +1,5 @@
 // Template object.
-var debug = 0;
+var debug = 1;
 
 try {
     exports.test = function() {
@@ -22,9 +22,8 @@ try {
     }
 }
 
-
 String.prototype.trim = function() {
-    return this.replace(/^\s+|\s+$/g,"").replace(/^[\n|\r]+|[\n|\r]+$/g, "");
+    return this.replace(/^\s+|\s+$/g, "").replace(/^[\n|\r]+|[\n|\r]+$/g, "");
 }
 
 var pad = function(count) {
@@ -35,6 +34,16 @@ var pad = function(count) {
         index += 1;
     }
     return pad;
+}
+
+var filter = function(fun, list) {
+    new_list = [];
+    list.forEach( function(item) {
+        if (fun(item) != false) {
+            new_list.push(item);
+        }
+    });
+    return new_list;
 }
 
 var jsninja_errors = [];
@@ -52,6 +61,8 @@ var dump_errors = function() {
 var jsninja_template_classname = 'jst-template';
 var jsninja_data_classname = 'jst-data';
 var jsninja_target_classname = 'jst-target';
+var jsninja_target_classname_url = 'jst-target-url';
+var jsninja_target_classname_url_json = 'jst-target-url-json';
 var jsninja_target_rendered_classname = 'jst-rendered';
 
 var Environment = function() {
@@ -65,11 +76,13 @@ var Environment = function() {
         "!":"exec",
         "@":"ignore"
     }
+    
+    this.default_data = {};
 }
 
 var Template = function(string) {
     this.string = string;
-    this.environment = new Environment();
+    this.environment = null;
     this.blocks = [];
 }
 
@@ -105,18 +118,20 @@ Template.prototype.find_next_block = function() {
 }
 
 Template.prototype.compile = function() {
+    if (this.environment == null) {
+        this.environment = new Environment();
+    }
     this.working_string = ""+this.string;
 
     var last = 0;
-    var count = 0;
     var depth = 0;
     
-    while(last != -1 && count < 20) {
-        count += 1
+    while(last != -1) {
         last = this.find_next_block();
     }
     
     var f_code = [];
+    var in_func = 0;
     
     this.blocks.forEach( function(obj) {
         var type = obj[0];
@@ -127,14 +142,43 @@ Template.prototype.compile = function() {
             f_code.push("print(" + JSON.stringify(data) + ");\n" );
         } else if ( type == 'condition') {
             data = data.trim();
-            if (data.substring(0,2) == 'if' || data.substring(0, 3) == 'for' || data.substring(0, 5) == 'while') {
+            if (data.substring(0,2) == 'if' || data.substring(0, 5) == 'while') {
                 f_code.push( "\n " + pad(depth) );
                 f_code.push(data + " {\n");
+                depth += 1;
+            } else if (data.substring(0, 3) == 'for') {
+                var sp = filter(function(item) { 
+                    if (item == '' || item == '(' || item == ')') { 
+                        return false; 
+                    } else { 
+                        return true; 
+                    } 
+                }, data.split(' '));
+                var rest = sp.slice(3).join(' ').trim();
+                
+                if (rest[rest.length-1] == ')') {
+                    rest = rest.substring(0, rest.length-1);
+                }
+                
+                var value_name = sp[1].trim();
+                
+                if (value_name[0] == '(') {
+                    value_name = value_name.substring(1);
+                }
+                
+                f_code.push( "\n" + rest + ".forEach( function(" + value_name +") {" );
+                f_code.push( "\n " + pad(depth) );
+                in_func += 1;
                 depth += 1;
             } else if (data == 'end') {
                 depth -= 1;
                 f_code.push( pad(depth) );
-                f_code.push('}\n');
+                if (in_func > 0) {
+                    f_code.push('});\n');
+                    in_func -= 1;
+                } else {
+                    f_code.push('}\n');
+                }
             } else if (data.substring(0, 4) == 'else' || data.substring(0, 7) == 'else if') {
                 f_code.push( pad(depth-1) );
                 f_code.push( "} " + data + " {\n");
@@ -154,28 +198,39 @@ Template.prototype.compile = function() {
 Template.prototype.render = function(variables, missing_value_cb) {
     if (this.blocks.length == 0) {
         this.compile();
-    }
-    
-    var s = [];
-    try {
-        var data = JSON.parse(variables);
-    } catch (e) {
-        var data = variables;
+
+        var s = [];
+        try {
+            var data = JSON.parse(variables);
+        } catch (e) {
+            var data = variables;
+        }
+
+        s.push('(function() {\n');
+        s.push(' var ____output = [];\n');
+        s.push(' var print = function(data) { ____output.push(data); };\n');
+        
+        for( i in data ) {
+            if (typeof data[i] == 'object') {
+                s.push(" var " + i + " = JSON.parse(" + JSON.stringify(data[i]) + ");\n");
+            } else {
+                s.push(" var " + i + " = " + JSON.stringify(data[i]) + ";\n");
+            }
+        }
+
+        for( i in this.environment.default_data ) {
+            s.push(" var " + i + " = " + JSON.stringify(this.environment.default_data[i]) + ";\n");
+        }
+        
+        s.push( '\n')
+        s.push( this.f_code_render );
+        s.push("\n\n return ____output.join('');\n})()");
+
+        this.compiled_code = s;
+        jsw('code', s.join(''));
     }
 
-    s.push('(function() {\n');
-    s.push(' var ____output = [];\n');
-    s.push(' var print = function(data) { ____output.push(data); };\n');
-    for( i in data ) {
-        s.push(" var " + i + " = " + JSON.stringify(data[i]) + ";\n");
-    }
-    s.push( '\n')
-    s.push( this.f_code_render );
-    s.push("\n\n return ____output.join('');\n})()");
-
-    jsw('code', s.join(''));
-
-    return eval(s.join(''));
+    return eval(this.compiled_code.join(''));
 }
 
 
@@ -184,41 +239,44 @@ Template.prototype.describe = function() {
     return { blocks: this.blocks };
 }
 
-var WebRenderer = function(document) {
-    this.document = document;
-    this.default_data = {};
+var WebRenderer = function() {
     this.template_dict = {};
+    this.environment = new Environment();
 }
 
 WebRenderer.prototype.create_template = function(key, value) {
-    var t = new jsninja.Template(value);
+    var t = new Template(value);
+    t.environment = this.environment;
     this.template_dict[key] = t;
 }
 
 WebRenderer.prototype.render_template = function(name, variables) {
     var t = this.template_dict[name];
-    return t.render(variables, this.get_default_value);
-}
-
-WebRenderer.prototype.get_default_value = function(key) {
-    return self.default_data[key]
+    try {
+        return t.render(variables, this.get_default_value);
+    } catch (e) {
+        return e;
+    }
 }
 
 WebRenderer.prototype.run_moo = function() {
     var self = this;
     var datas = $$("." + jsninja_data_classname)
-    var default_data = this.default_data;
+    var defaults = {};
+    
     // This will require a lib of some kind.
     datas.forEach( function(obj) {
         try {
             var d = JSON.parse(obj.innerHTML);
             for( key in d ) {
-                default_data[key] = d[key];
+                defaults[key] = d[key];
             }
         } catch (e) {
             jsninja_errors.push( [obj, e] )
         }
     });
+
+    this.environment.default_data = defaults;
     
     var templates = $$("." + jsninja_template_classname);
     var template_dict = this.template_dict;
@@ -243,7 +301,91 @@ WebRenderer.prototype.run_moo = function() {
         obj.className = class_names.join(' ') + " " + jsninja_target_rendered_classname;
     });
     
+    var targets = $$("." + jsninja_target_classname_url);
+    
+    targets.forEach( function( obj ) {
+        (function() {
+            var classes = obj.className.split(' ');
+            var url = obj.innerHTML;
+
+            var myRequest = new Request(
+                {
+                    url: url,
+                    onSuccess: function(responseText, responseXML) {
+                        var data = JSON.parse(responseText);
+                        var class_names = [];
+                        classes.forEach( function( name ) {
+                            if (name != jsninja_target_classname_url) {
+                                var tr = self.render_template(name, data);
+                                obj.innerHTML = tr;
+                                class_names.push(name);
+                            }
+                        });
+                        obj.className = class_names.join(' ') + " " + jsninja_target_rendered_classname;
+                    },
+                    onFailure: function(obj) {
+                        alert("Failure: " + obj);
+                    }, 
+                    onException: function(headerName, value) {
+                        alert("Exception: " + value);
+                    }
+                }
+            );
+            myRequest.get();
+        })();
+    });    
+
+
+    var targets = $$("." + jsninja_target_classname_url_json);
+    
+    targets.forEach( function( obj ) {
+        (function() {
+            var classes = obj.className.split(' ');
+            var header = JSON.parse(obj.innerHTML);
+            obj.innerHTML = header['loading'];
+            var url = header['url'];
+
+            var myRequest = new Request(
+                {
+                    url: url,
+                    onSuccess: function(responseText, responseXML) {
+                        var data = JSON.parse(responseText);
+                        jsw('decode', data);
+                        var class_names = [];
+                        classes.forEach( function( name ) {
+                            if (name != jsninja_target_classname_url_json) {
+                                var tr = self.render_template(name, data);
+                                obj.innerHTML = tr;
+                                class_names.push(name);
+                            }
+                        });
+                        obj.className = class_names.join(' ') + " " + jsninja_target_rendered_classname;
+                    },
+                    onFailure: function(obj) {
+                        alert("Failure: " + obj);
+                    }, 
+                    onException: function(headerName, value) {
+                        alert("Exception: " + value);
+                    }
+                }
+            );
+            myRequest.get();
+        })();
+    });
+
     dump_errors();
+}
+
+WebRenderer.prototype.reload = function() {
+    this.template_dict = {};
+    this.environment = new Environment();
+}
+
+var update_dom_dict = function(id, da) {
+    var d = $(id);
+    var data = JSON.parse(d.innerHTML);
+    data.update(da);
+    d.innerHTML = JSON.stringify(data);
 }
 
 try {
---
+++ readme.txt
@@ -0,0 +1,9 @@
+jsninja
+-------
+
+purpose:
+	Javascript is a nice language, i've been doing some work with NodeJS and I couldn't find any good Javascript templating engines. I think this is partly because there isn't a good workflow for doing these sorts of things.
+
+So i've gone ahead and attempted to set one up.
+
+Rendering templates server side can be really nice, it means that you have really good data locality, however, you have to do a good amount of work on the server each time you want to render a page. 
