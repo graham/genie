4abee471751cea0debc85e2ee6eeeee075fe335d
graham <graham.abbott@gmail.com>
2012-09-14 16:56:02 -0700

    added python

---
+++ python/genie.py
@@ -0,0 +1,5 @@
+GENIE_VERSION = '0.2.py'
+GENIE_CONTEXT_begin = "{"
+GENIE_CONTEXT_end = "}"
+
+GENIE_CONTEXT_lookup = { "#":"comment", "%":"condition", "!":"exec", "&":"bindable", "^":"notes", "~":"compiler" }; 
