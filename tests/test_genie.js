2210a5b9bdfc2da267ae6399c2ceecc8232a86ed
graham <graham.abbott@gmail.com>
2013-06-24 02:03:54 -0700

    gotta add some tests.

---
+++ tests/test_genie.js
@@ -0,0 +1,33 @@
+describe("Template", function() {
+  var template = null;
+
+  beforeEach(function() {
+      template = new genie.Template('My name is [[v.name]].');
+  });
+
+  it("should be able to render correctly.", function() {
+      expect(template.render({"name":"graham"})).toEqual("My name is graham.");
+  });
+
+  it("single if statements work.", function() {
+      var myTemplate = new genie.Template("[% if v.question %]Yes[% end %]");
+      expect(myTemplate.render({"question":true})).toEqual("Yes");
+  });
+
+  it("if statements and else work.", function() {
+      var myTemplate = new genie.Template("[% if v.question %]Yes[% else %]No[% end %]");
+      expect(myTemplate.render({"question":true})).toEqual("Yes");
+      expect(myTemplate.render({"question":false})).toEqual("No");
+  });
+
+  it("elif statements and else work.", function() {
+      var myTemplate = new genie.Template("[% if v.question %]Yes[% else if 1 %]Middle[% else %]No[% end %]");
+      expect(myTemplate.render({"question":false})).toEqual("Middle");
+  });
+
+  it("exposed statements should be available without v.", function() {
+      var myTemplate = new genie.Template('[^ expose ["name"] ^]My name is [[ name ]].[^ expose "word" ^]');
+      expect(myTemplate.render({"name":"graham"})).toEqual("My name is graham.");
+  });
+
+});
