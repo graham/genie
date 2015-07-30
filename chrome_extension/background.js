dd7fa07b919bac7efd9cdd806f207a9920b9834c
graham <graham.abbott@gmail.com>
2015-07-30 21:39:45 +0100

    popup now works and talks with the backend, next step is to add in some commands, searches work, !actions should call a javascript function.

--- chrome_extension/background.js
+++ chrome_extension/background.js
@@ -1,5 +1,9 @@
 var last = null;
 var genie_env = new genie.Environment();
+var search = {active:true, currentWindow: true};
+
+var fake_results = ['Aardvark', 'Albatross', 'Alligator', 'Alpaca', 'Ant', 'Anteater', 'Antelope', 'Ape', 'Armadillo', 'Baboon', 'Badger', 'Barracuda', 'Bat', 'Bear', 'Beaver', 'Bee', 'Bison', 'Boar', 'Buffalo', 'Butterfly', 'Camel', 'Capybara', 'Caribou', 'Cassowary', 'Cat', 'Caterpillar', 'Chamois', 'Cheetah', 'Chicken', 'Chimpanzee', 'Chinchilla', 'Chough', 'Clam', 'Cobra', 'Cockroach', 'Cod', 'Cormorant', 'Coyote', 'Crab', 'Crane', 'Crocodile', 'Crow', 'Curlew', 'Deer', 'Dinosaur', 'Dog', 'Dogfish', 'Dolphin', 'Donkey', 'Dotterel', 'Dove', 'Dragonfly', 'Duck', 'Dugong', 'Dunlin', 'Eagle', 'Echidna', 'Eel', 'Eland', 'Elephant', 'Elephantseal', 'Elk', 'Emu', 'Falcon', 'Ferret', 'Finch', 'Fish', 'Flamingo', 'Fly', 'Fox', 'Frog', 'Gaur', 'Gazelle', 'Gerbil', 'GiantPanda', 'Giraffe', 'Gnat', 'Gnu', 'Goat', 'Goldfish', 'Goose', 'Gorilla', 'Goshawk', 'Grasshopper', 'Grouse', 'Guanaco', 'Guineapig', 'Gull', 'Hamster', 'Hare', 'Hawk', 'Hedgehog', 'Heron', 'Herring', 'Hippopotamus', 'Hornet', 'Horse', 'Human', 'Hummingbird', 'Hyena', 'Ibex', 'Ibis', 'Jackal', 'Jaguar', 'Jay', 'Bluejay', 'Jellyfish', 'Kangaroo', 'Kingfisher', 'Koala', 'Komododragon', 'Kookabura', 'Kouprey', 'Kudu', 'Lapwing', 'Lark', 'Lemur', 'Leopard', 'Lion', 'Llama', 'Lobster', 'Locust', 'Loris', 'Louse', 'Lyrebird', 'Magpie', 'Mallard', 'Manatee', 'Mandrill', 'Mantis', 'Marten', 'Meerkat', 'Mink', 'Mole', 'Mongoose', 'Monkey', 'Moose', 'Mosquito', 'Mouse', 'Mule', 'Narwhal', 'Newt', 'Nightingale', 'Octopus', 'Okapi', 'Opossum', 'Oryx', 'Ostrich', 'Otter', 'Owl', 'Oyster', 'Panda', 'Panther', 'Parrot', 'Partridge', 'Peafowl', 'Pelican', 'Penguin', 'Pheasant', 'Pig', 'Pigeon', 'PolarBear', 'Pony', 'Porcupine', 'Porpoise', 'PrairieDog', 'Quail', 'Quelea', 'Quetzal', 'Rabbit', 'Raccoon', 'Rail', 'Ram', 'Rat', 'Raven', 'Redpanda', 'Reindeer', 'Rhinoceros', 'Rook', 'Salamander', 'Salmon', 'SandDollar', 'Sandpiper', 'Sardine', 'Scorpion', 'Sealion', 'SeaUrchin', 'Seahorse', 'Seal', 'Shark', 'Sheep', 'Shrew', 'Skunk', 'Snail', 'Snake', 'Sparrow', 'Spider', 'Spoonbill', 'Squid', 'Squirrel', 'Starling', 'Stingray', 'Stinkbug', 'Stork', 'Swallow', 'Swan', 'Tapir', 'Tarsier', 'Termite', 'Tiger', 'Toad', 'Trout', 'Turkey', 'Turtle', 'Viper', 'Vulture', 'Wallaby', 'Walrus', 'Wasp', 'Weasel', 'Whale', 'Wildcat', 'Wolf', 'Wolverine', 'Wombat', 'Woodcock', 'Woodpecker', 'Worm', 'Wren', 'Yak', 'Zebra'];
+                    
 
 function update_status() {
     if (items_to_sync.length > 0) {
@@ -11,7 +15,7 @@ chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         if (request.command == 'render-as-template') {
             var result = genie.fs(request.content, {"engine":"Genie 0.5"});
-            chrome.tabs.query({currentWindow: true}, function(tabs) {
+            chrome.tabs.query(search, function(tabs) {
                 chrome.tabs.sendMessage(tabs[0].id, {command:"replace-active-content-with",
                                                      content:result}, function(response) {
                     console.log("sent a result to the main window.");
@@ -19,12 +23,27 @@ chrome.runtime.onMessage.addListener(
             });
         } else if (request.command == 'render-active-with-template') {
             var result = genie_env.render(request.template_name, {});
-            chrome.tabs.query({currentWindow: true}, function(tabs) {
+            chrome.tabs.query(search, function(tabs) {
                 chrome.tabs.sendMessage(tabs[0].id, {command:"replace-active-content-with",
                                                      content:result}, function(response) {
                     console.log("sent a result to the main window.");
                 });
             });
+        } else if (request.command == 'quick-search') {
+            if (request.query.length == 0) {
+                sendResponse({"results":[]});
+            } else {
+                var results = [];
+
+                for(var i = 0; i < fake_results.length; i++) {
+                    var row = fake_results[i];
+                    row = row.toLowerCase();
+                    if (row.slice(0, request.query.length) == request.query) {
+                        results.push(fake_results[i]);
+                    }
+                }
+                sendResponse({'results':results});
+            }
         } else {
             var url = "http://github.com/graham/genie";
             chrome.tabs.create({ active:false, url: url });
@@ -35,13 +54,13 @@ chrome.runtime.onMessage.addListener(
 (function() {
 	chrome.commands.onCommand.addListener(function(command) {
         if (command == "render-focused-editable") {
-            chrome.tabs.query({currentWindow: true}, function(tabs) {
+            chrome.tabs.query(search, function(tabs) {
                 chrome.tabs.sendMessage(tabs[0].id, {command: "request-active-content"}, function(response) {
                     console.log("requested content.");
                 });
             });
         } else if (command == "open-drawer") {
-            chrome.tabs.query({currentWindow: true}, function(tabs) {
+            chrome.tabs.query(search, function(tabs) {
                 chrome.tabs.sendMessage(tabs[0].id, {command: "show-omni-bar"}, function(response) {
                     console.log("Requested Omni bar show for tab: " + tabs[0]);
                 });
--- chrome_extension/content.js
+++ chrome_extension/content.js
@@ -1,9 +1,71 @@
 (function() {
+    var singleton = null;
+    var remove_from_page = function() {
+        setTimeout(function() {
+            if (singleton) {
+                document.body.removeChild(singleton[0]);
+                singleton = null;
+                if (previous_focus) {
+                    setTimeout(function() {
+                        previous_focus.focus();
+                        previous_focus = null;
+                    }, 1);
+                }
+            }
+        }, 1);
+    };
+
+    var previous_focus = null;
+    
+    var style = 'position: fixed; top: 20%; width: 60%; left: 20%; font-family: serif; margin:auto; background-color: rgba(222, 255, 222, 0.8);'
+    
     chrome.runtime.onMessage.addListener(
         function(request, sender, sendResponse) {
-            console.log(request);
             if (request.command == 'show-omni-bar') {
+                if (singleton != null) {
+                    //singleton[1].focus();
+                    remove_from_page();
+                } else {
+                    previous_focus = document.activeElement;
+                    var d = document.createElement('div');
+                    var form = document.createElement('form');
+                    var input = document.createElement('input');
+                    var ul = document.createElement('div');
                 
+                    input.style.cssText = "width: 100%; padding: 20px; font-size:40px;background:transparent;outline:none;border:0;";
+                    input.type = 'textfield';
+                
+                    d.appendChild(form);
+                    form.appendChild(input);
+                
+                    d.style.cssText = style;
+                    d.appendChild(ul);
+                    document.body.appendChild(d);
+                
+                    form.onsubmit = remove_from_page;
+                    input.onblur = remove_from_page;
+
+                    input.onkeyup = function() {
+                        chrome.runtime.sendMessage({command:"quick-search",
+                                                    query:input.value},
+                                                   function(response) {
+                            console.log(response);
+                            ul.innerHTML = '';
+                            for(var i = 0; i < response.results.length; i++) {
+                                var obj = response.results[i];
+                                var ii = document.createElement('div');
+                                ii.innerHTML = obj;
+                                ul.appendChild(ii);
+                            }
+                        });
+                    };
+
+                    singleton = [d, input];
+                
+                    setTimeout(function() {
+                        input.focus();
+                    }, 1);
+                }
             } else if (request.command == 'request-active-content') {
                 var activeType = document.activeElement.type;
 
